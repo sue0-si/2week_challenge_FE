124ae0d6bfcbc7074e2de734b671d2e68919358e
slee3593 <slee3593@gatech.edu>
2023-11-21 20:25:12 +0900

    day6: carousel

---
+++ day6/slide.js
@@ -0,0 +1,25 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const carousel = document.querySelector('.images');
+    const slides = document.querySelectorAll('.image');
+    const slideWidth = slides[0].offsetWidth;
+
+    if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else if (index >= slides.length) {
+        currentIndex = 0;
+    } else {
+        currentIndex = index;
+    }
+    carousel.style.transform = `translateX(${-currentIndex * slideWidth}px)`;
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+setInterval(nextSlide, 2000);

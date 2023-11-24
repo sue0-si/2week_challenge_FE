72f89c0563d915f74fa1fd7bffd5530a6cad5ae1
slee3593 <slee3593@gatech.edu>
2023-11-24 23:18:37 +0900

    design: font, margin

--- day9/login.js
+++ day9/login.js
@@ -1,13 +1,27 @@
 function signUp() {
+    const userId = document.getElementById('signUpId').value;
+    const userPassword = document.getElementById('signUpPassword').value;
+
+    if (userId === '' || userPassword === '') {
+        alert('enter the id and password correctly');
+        return;
+    }
+
     const loginPage = document.getElementById('login');
     const signUpPage = document.getElementById('signUp');
     loginPage.style.display = 'flex';
     signUpPage.style.display = 'none';
+
 }
 
 function login() {
     const id = document.getElementById('userId').value;
     const password = document.getElementById('password').value;
+
+    if (id === '' || password === '') {
+        alert('enter the id and password correctly');
+        return;
+    }
 
     const userId = document.getElementById('signUpId').value;
     const userPassword = document.getElementById('signUpPassword').value;
@@ -17,5 +31,5 @@ function login() {
     } else {
         alert('ID or password is invalid')
     }
-
 }
+
